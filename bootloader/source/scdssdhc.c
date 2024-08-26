e1b74603cf8271ad919cfbcbac0390e24abf5208
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-08-26 00:22:26 -0700

    bootloader: update scdssdhc code

--- bootloader/source/scdssdhc.c
+++ bootloader/source/scdssdhc.c
@@ -11,8 +11,7 @@
 #include "scdssdhc.h"
 #include "libtwl_card.h"
 
-static int initialized = 0;
-int isSDHC = 0;
+u32 isSDHC = 0;
 
 static void SCDS_ReadCardData(u64 command, u32 flags, void *buffer, u32 length)
 {
@@ -39,23 +38,23 @@ static u32 SCDS_IsSDHostBusy(void)
 	return SCDS_SendCommand(SCDS_CMD_SD_HOST_BUSY, 0);
 }
 
-static void SCDS_SDSendR0Command(u8 sdio, u32 parameter)
+static void SCDS_SDSendR0Command(u8 sdio, u32 parameter, u32 latency)
 {
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_NORESPONSE), initialized ? 0 : 0x1000);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_NORESPONSE), latency);
 	while(SCDS_IsSDHostBusy());
 }
 
-static u32 SCDS_SDSendR1Command(u8 sdio, u32 parameter)
+static u32 SCDS_SDSendR1Command(u8 sdio, u32 parameter, u32 latency)
 {
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_READ_4B), initialized ? 0 : 0x1000);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_READ_4B), latency);
 	while(SCDS_IsSDHostBusy());
 	return __builtin_bswap32(SCDS_SendCommand(SCDS_CMD_SD_HOST_RESPONSE, 0));
 }
 
 // TODO: save the response to a buffer (also figure out in which order they're sent)
-static void SCDS_SDSendR2Command(u8 sdio, u32 parameter)
+static void SCDS_SDSendR2Command(u8 sdio, u32 parameter, u32 latency)
 {
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_READ_4B_MULTI), initialized ? 0 : 0x1000);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_READ_4B_MULTI), latency);
 	while(SCDS_IsSDHostBusy());
 
 	// TODO: parse this response
@@ -63,53 +62,60 @@ static void SCDS_SDSendR2Command(u8 sdio, u32 parameter)
 
 	for(int i=0; i < 4; i++)
 	{
-		SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_NEXT_4B), initialized ? 0 : 0x1000);
+		SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_NEXT_4B), latency);
 		while(SCDS_IsSDHostBusy());
 		// TODO: parse this response
 		SCDS_SendCommand(SCDS_CMD_SD_HOST_RESPONSE, 0);
 	}
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_SEND_CLK), 0);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(sdio, parameter, SCDS_SD_HOST_SEND_STOP_CLK), 0);
 	while(SCDS_IsSDHostBusy());
 }
+
+void swiDelay(u32 count);
 
 static void SCDS_SDSetHostRegister(u8 bits)
 {
 	SCDS_SendCommand(SCDS_CMD_SD_HOST_SET_REGISTER(bits), 0);
+	swiDelay(0x300);
 }
 
 void SCDS_SDGetSDHCStatusFromSRAM(void)
 {
-	initialized = 1;
 	isSDHC = SCDS_SendCommand(SCDS_CMD_SRAM_READ_DATA(0x7F9E0), 0) != 0 ? 1 : 0;
 }
 
 u32 SCDS_SendCommand(const u64 command, u32 latency)
 {
-	u32 ret;
-	SCDS_ReadCardData(command, SCDS_CTRL_READ_4B | MCCNT1_LATENCY1(latency), &ret, 1);
-	return ret;
+	card_romSetCmd(command);
+	card_romStartXfer(SCDS_CTRL_READ_4B | MCCNT1_LATENCY1(latency), false);
+	card_romWaitDataReady();
+	return card_romGetData();
 }
 
 bool SCDS_SDInitialize(void)
 {
-	int isSD20 = 0;
+	u32 isSD20 = 0;
 	u32 response = 0;
 
-	initialized = 0;
 	isSDHC = 0;
 
 	// TODO: What is this command doing?
 	SCDS_ReadCardData(0x6600000000000000ull, 0xA7586000, &response, 1);
 
-	SCDS_SDSetHostRegister(SCDS_SD_HOST_REG_RESET | SCDS_SD_HOST_REG_400KHZ_CLK | SCDS_SD_HOST_REG_CLEAN_ROM_MODE);
+	// Reset SD host
+	SCDS_SDSetHostRegister(0);
 
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(0, 0, SCDS_SD_HOST_SEND_CLK), 0x1000);
+	// Init
+	SCDS_SDSetHostRegister(SCDS_SD_HOST_REG_RESET | SCDS_SD_HOST_REG_400KHZ_CLK | SCDS_SD_HOST_REG_CLEAN_ROM_MODE);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(0, 0, SCDS_SD_HOST_SEND_CLK), SCDS_CTRL_SD_LOW_CLK_LATENCY);
+	swiDelay(0x1000);
 
 	// CMD0
-	SCDS_SDSendR0Command(0, 0);
+	SCDS_SDSendR0Command(0, 0, SCDS_CTRL_SD_LOW_CLK_LATENCY);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(0, 0, SCDS_SD_HOST_SEND_STOP_CLK), SCDS_CTRL_SD_LOW_CLK_LATENCY);
 
 	// CMD8
-	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(8, 0x1AA, SCDS_SD_HOST_READ_4B), 0x1000);
+	SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(8, 0x1AA, SCDS_SD_HOST_READ_4B), SCDS_CTRL_SD_LOW_CLK_LATENCY);
 
 	u32 retryCount = 9999;
 	while(1)
@@ -134,7 +140,7 @@ bool SCDS_SDInitialize(void)
 	do
 	{
 		// CMD55
-		SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(55, 0, SCDS_SD_HOST_READ_4B), 0x1000);
+		SCDS_SendCommand(SCDS_CMD_SD_HOST_PARAM(55, 0, SCDS_SD_HOST_READ_4B), SCDS_CTRL_SD_LOW_CLK_LATENCY);
 		retryCount = 9999;
 		while(SCDS_IsSDHostBusy())
 		{
@@ -151,31 +157,32 @@ bool SCDS_SDInitialize(void)
 		u32 parameter = 0x00FC0000;
 		if(isSD20)
 			parameter |= BIT(30);
-		response = SCDS_SDSendR1Command(41, parameter);
+		response = SCDS_SDSendR1Command(41, parameter, SCDS_CTRL_SD_LOW_CLK_LATENCY);
 	} while(!(response & BIT(31)));
 
 	isSDHC = response & BIT(30) ? 1 : 0;
 
 	// CMD2
-	SCDS_SDSendR2Command(2, 0);
+	SCDS_SDSendR2Command(2, 0, SCDS_CTRL_SD_LOW_CLK_LATENCY);
 
 	// CMD3
-	response = SCDS_SDSendR1Command(3, 0);
+	response = SCDS_SDSendR1Command(3, 0, SCDS_CTRL_SD_LOW_CLK_LATENCY);
 	u32 sdio_rca = response & 0xFFFF0000;
 
 	// CMD7
-	SCDS_SDSendR1Command(7, sdio_rca);
+	SCDS_SDSendR1Command(7, sdio_rca, SCDS_CTRL_SD_LOW_CLK_LATENCY);
 
 	// ACMD6
-	SCDS_SDSendR1Command(55, sdio_rca);
-	SCDS_SDSendR1Command(6, 2);
+	SCDS_SDSendR1Command(55, sdio_rca, SCDS_CTRL_SD_LOW_CLK_LATENCY);
+	SCDS_SDSendR1Command(6, 2, SCDS_CTRL_SD_LOW_CLK_LATENCY);
+
+	// CMD16
+	SCDS_SDSendR1Command(16, 512, SCDS_CTRL_SD_LOW_CLK_LATENCY);
 
 	SCDS_SDSetHostRegister(0);
 	SCDS_SDSetHostRegister(SCDS_SD_HOST_REG_RESET | SCDS_SD_HOST_REG_CLEAN_ROM_MODE);
 	if(isSDHC)
 		SCDS_SDSetHostRegister(SCDS_SD_HOST_REG_RESET | SCDS_SD_HOST_REG_CLEAN_ROM_MODE | SCDS_SD_HOST_REG_SDHC);
-
-	initialized = 1;
 
 	return true;
 }
@@ -212,13 +219,13 @@ void SCDS_SDReadMultiSector(u32 sector, void *buffer, u32 num_sectors)
 	};
 
 	// end read
-	SCDS_SDSendR1Command(12, 0);
+	SCDS_SDSendR1Command(12, 0, 0);
 }
 
 void SCDS_SDWriteSingleSector(u32 sector, const void *buffer)
 {
 	// instruct cart where to write
-	SCDS_SDSendR1Command(24, isSDHC ? sector : sector << 9);
+	SCDS_SDSendR1Command(24, isSDHC ? sector : sector << 9, 0);
 
 	// write
 	SCDS_WriteCardData(SCDS_CMD_FIFO_WRITE_DATA, SCDS_CTRL_WRITE_512B, buffer, 128);
@@ -232,10 +239,9 @@ void SCDS_SDWriteSingleSector(u32 sector, const void *buffer)
 void SCDS_SDWriteMultiSector(u32 sector, const void *buffer, u32 num_sectors)
 {
 	// instruct cart where to write
-	SCDS_SDSendR1Command(25, isSDHC ? sector : sector << 9);
+	SCDS_SDSendR1Command(25, isSDHC ? sector : sector << 9, 0);
 
-	do
-	{
+	while (num_sectors--) {
 		// end write
 		// well, it's supposed to be end write. But doing it first is a no-op, and it's also
 		// a little simpler to write this way
@@ -245,11 +251,10 @@ void SCDS_SDWriteMultiSector(u32 sector, const void *buffer, u32 num_sectors)
 		SCDS_WriteCardData(SCDS_CMD_FIFO_WRITE_DATA, SCDS_CTRL_WRITE_512B, buffer, 128);
 		while(SCDS_IsSDHostBusy());
 		buffer = (u8 *)buffer + 0x200;
-		num_sectors--;
-	} while (num_sectors);
+	}
 
 	// *really* end write
-	SCDS_SDSendR1Command(12, 0);
+	SCDS_SDSendR1Command(12, 0, 0);
 	SCDS_SendCommand(SCDS_CMD_SD_WRITE_END, 0);
 	while(SCDS_IsSDHostBusy());
 }
